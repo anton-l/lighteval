df4a8f38c3fbd45a60f40650fa1c2134e55e80dc
anton <anton@huggingface.co>
2024-12-19 10:12:38 +0100

    fix repeated cleanup

--- src/lighteval/models/vllm_model.py
+++ src/lighteval/models/vllm_model.py
@@ -164,7 +164,7 @@ class VLLMModel(LightevalModel):
             config.pretrained,
             tokenizer_mode="auto",
             trust_remote_code=config.trust_remote_code,
-            tokenizer_revision=config.revision,
+            revision=config.revision,
         )
         tokenizer.pad_token = tokenizer.eos_token
         return tokenizer
